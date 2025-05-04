3310a1d165c61753543ccf92f13196a23d064103
acassaigne <anthony.cassaigne@gmail.com>
2025-05-04 19:00:35 +0200

    rename class with abstract inheritance

--- app/src/test/kotlin/org/example/JunitInterfaceTest.kt
+++ app/src/test/kotlin/org/example/JunitInterfaceTest.kt
@@ -9,7 +9,7 @@ abstract class ContractTestsForAdapter {
     abstract fun secondTest ();
 }
 
-class WithJunitTestOverAbstract : ContractTestsForAdapter() {
+class WithJunitTest : ContractTestsForAdapter() {
 
     @Test
     override fun firstTest() {
